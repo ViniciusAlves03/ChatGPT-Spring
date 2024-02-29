a7478e44287cd513292c803e61efd7e6a8c6246e
Vinícius Alves <vinicius.alves0503@gmail.com>
2024-02-29 20:56:57 -0300

    Criando classes de configuração
    - Criando Classe de request
    - Adicionando RestTemplate
    - Criando VO para request

---
+++ src/main/java/com/gpt/chatgptspringintegration/config/OpenAIConfig.java
@@ -0,0 +1,27 @@
+package com.gpt.chatgptspringintegration.config;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class OpenAIConfig {
+
+    private Logger logger = Logger.getLogger(OpenAIConfig.class.getName());
+
+    @Value("${openai.api.key}")
+    String openApiKey;
+
+    @Bean
+    RestTemplate template(){
+        logger.info("Initializing RestTemplate");
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            request.getHeaders().add("Authorization", "Bearer " + openApiKey);
+            return execution.execute(request, body);
+        });
+        return restTemplate;
+    }
+}
