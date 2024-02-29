81a0842b106695e042a6d297e6123b0d9dc6cb4b
Vinícius Alves <vinicius.alves0503@gmail.com>
2024-02-29 20:21:11 -0300

    Adicionando arquivos Spring

---
+++ src/main/java/com/gpt/chatgptspringintegration/ChatgptSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.gpt.chatgptspringintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptSpringIntegrationApplication.class, args);
+	}
+
+}
